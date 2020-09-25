9fbc16f4c9a1539bd4dc80512f8306da704da6c4
kirsebom <olemartinkirsebom@gmail.com>
2020-09-25 13:16:11 +0200

    Module assignment 2

---
+++ script.js
@@ -0,0 +1,53 @@
+// question 1
+
+var petArray = ["cat", "cow", "dog"];
+
+//question 2
+
+console.log(petArray[1]);
+
+// question 3
+
+petArray.push("sheep");
+
+//question 4
+
+console.log(petArray.length);
+
+// question 5
+
+var catObject = {
+	name: "Alfred",
+	colour: "black",
+	age: 7,
+};
+
+// question 6
+
+console.log(catObject.age);
+
+// question 7
+
+var catArray = [
+	{
+		name: "Alfred",
+		colour: "Black",
+		age: 7,
+	},
+];
+
+// question 8
+
+for (var i = 0; i < catArray.length; i++) {
+	console.log(catArray[i]);
+}
+
+// question 9
+
+function logToConsole(animal) {
+	console.log(animal);
+}
+
+//question 10
+
+logToConsole("donkey");
